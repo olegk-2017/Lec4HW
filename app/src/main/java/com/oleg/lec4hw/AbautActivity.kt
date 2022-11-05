2f14d70d90d407e58c8d4d933700af33fb32889b
Oleg K <Olegkovalinsky@gmail.com>
2022-11-06 01:59:13 +0200

    first commit

---
+++ app/src/main/java/com/oleg/lec4hw/AbautActivity.kt
@@ -0,0 +1,11 @@
+package com.oleg.lec4hw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AbautActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_abaut)
+    }
+}
