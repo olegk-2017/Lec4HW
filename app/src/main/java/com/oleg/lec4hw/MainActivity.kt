d1455c02160e54a23371f9273d8326805e7f82b1
Oleg K <Olegkovalinsky@gmail.com>
2022-11-06 02:21:09 +0200

    first commit

--- app/src/main/java/com/oleg/lec4hw/MainActivity.kt
+++ app/src/main/java/com/oleg/lec4hw/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.oleg.lec4hw
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ForceLightThemeMode() {
+    lateinit var btnAbout:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnAbout = findViewById(R.id.btn_about)
+        btnAbout.setOnClickListener{
+            val aboutIntent = Intent(this,AboutActivity::class.java)
+            startActivity(aboutIntent)
+        }
     }
 }
